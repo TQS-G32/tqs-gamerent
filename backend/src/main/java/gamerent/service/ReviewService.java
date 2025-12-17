f0c4540573044b681a414e0975be6695b364a1ce
FilipeNV1 <filipeviseu@ua.pt>
2025-12-17 23:00:53 +0000

    small fix

--- backend/src/main/java/gamerent/service/ReviewService.java
+++ backend/src/main/java/gamerent/service/ReviewService.java
@@ -18,7 +18,6 @@ import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @Service
 public class ReviewService {
